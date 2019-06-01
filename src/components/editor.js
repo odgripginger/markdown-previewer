96a87c1a45ea0efe382d57a201de07c1eee83dfa
Om Dhiraj <omdhiraj55@gmail.com>
2019-06-02 02:30:49 +0530

    basic app completed

--- src/components/editor.js
+++ src/components/editor.js
@@ -1,10 +1,17 @@
 import React from "react";
 const Editor = props => {
   return (
-    <div>
-      <textarea id="editor" rows="20" cols="50" onChange={props.onChange}>
-        {props.markdown}
-      </textarea>
+    <div id="editBox">
+      <div className="toolbar">
+        <h3>Editor</h3>
+      </div>
+      <textarea
+        id="editor"
+        rows="20"
+        cols="50"
+        value={props.markdown}
+        onChange={props.onChange}
+      />
     </div>
   );
 };
--- src/components/preview.js
+++ src/components/preview.js
@@ -1,12 +1,25 @@
 import React, { Component } from "react";
 import marked from "marked";
 
+marked.setOptions({ breaks: true });
+
 const renderer = new marked.Renderer();
+renderer.link = function(href, title, text) {
+  return `<a target="_blank" href="${href}">${text}` + "</a>";
+};
 export default class Preview extends Component {
   render() {
     const output = this.props.markdown;
     return (
-      <div dangerouslySetInnerHTML={{ __html: marked(output, { renderer }) }} />
+      <div id="previewBox">
+        <div className="toolbar">
+          <h3>Preview</h3>
+        </div>
+        <div
+          id="preview"
+          dangerouslySetInnerHTML={{ __html: marked(output, { renderer }) }}
+        />
+      </div>
     );
   }
 }
